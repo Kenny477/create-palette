cf8df24445aca20132effd63efd5856aedba851a
Kenny477 <53405834+Kenny477@users.noreply.github.com>
2021-11-05 21:39:49 -0700

    Working on neutral color exclusion option

--- palette.py
+++ palette.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import cv2
 from PIL import Image
 from sklearn.cluster import KMeans
 import numpy as np
+
 class Palette:
 
     CLUSTERS = None
@@ -11,24 +10,21 @@ class Palette:
     COLORS = None
     LABELS = None
     
-    def __init__(self, image, clusters=3):
-        self.IMAGE = image
+    def __init__(self, image, clusters: int = 3, downsize: float = 1.00):
+        img = Image.open(image)
+        img = img.resize((round(img.size[0]*downsize), round(img.size[1]*downsize)), Image.LANCZOS)
+        self.IMAGE = img
         self.CLUSTERS = clusters
     
     def rgb_to_hex(self, rgb):
         return '#%02x%02x%02x' % (int(rgb[0]), int(rgb[1]), int(rgb[2]))
 
-    def dominantColors(self):
+    def dominantColors(self, neutral=True):
         #read image
-        img = cv2.imread(self.IMAGE)
-        
-        #convert to rgb from bgr
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        
+        img = np.array(self.IMAGE)
+
         #reshaping to a list of pixels
         img = img.reshape((img.shape[0] * img.shape[1], 3))
-
-        self.IMAGE = img
 
         #using k-means to cluster pixels
         kmeans = KMeans(n_clusters = self.CLUSTERS, random_state=477)
@@ -43,71 +39,52 @@ class Palette:
         #returning after converting to integer from float
         return self.COLORS.astype(int)
     
-    def plotClusters(self):
+    def plotClusters(self, save=''):
         #plotting 
         fig = plt.figure()
-        ax = Axes3D(fig)
-        for label, pix in zip(self.LABELS, self.IMAGE):
+        ax = fig.add_subplot(projection='3d')
+        img = np.array(self.IMAGE)
+        img = img.reshape((img.shape[0] * img.shape[1], 3))
+        for label, pix in zip(self.LABELS, img):
             ax.scatter(pix[0], pix[1], pix[2], color = self.rgb_to_hex(self.COLORS[label]))
-        plt.show()
+        if save:
+            plt.savefig(save)
+        else:
+            plt.show()
     
-    def plotHistogram(self):
-       
-        #labels form 0 to no. of clusters
+    def drawPalette(self, size: int = 100, save='') -> Image.Image:
         numLabels = np.arange(0, self.CLUSTERS+1)
-       
-        #create frequency count tables    
+         
         (hist, _) = np.histogram(self.LABELS, bins = numLabels)
-        hist = hist.astype("float")
+        hist = hist.astype(float)
         hist /= hist.sum()
         
-        #appending frequencies to cluster centers
         colors = self.COLORS
-        
-        #descending order sorting as per frequency count
+
         colors = colors[(-hist).argsort()]
         hist = hist[(-hist).argsort()] 
         
-        #creating empty chart
-        chart = np.zeros((50, 500, 3), np.uint8)
-        start = 0
-        
-        #creating color rectangles
-        for i in range(self.CLUSTERS):
-            end = start + hist[i] * 500
-            
-            #getting rgb values
-            r = colors[i][0]
-            g = colors[i][1]
-            b = colors[i][2]
-            
-            #using cv2.rectangle to plot colors
-            cv2.rectangle(chart, (int(start), 0), (int(end), 50), (r,g,b), -1)
-            start = end	
-        
-        #display chart
-        plt.figure()
-        plt.axis("off")
-        plt.imshow(chart)
-        plt.show()
+        colors = colors.astype(int)
 
-    def drawPalette(self, size: int = 100) -> Image.Image:
         img = Image.new("RGB", (size*self.CLUSTERS, size))
-        for index, color in enumerate(self.COLORS.astype(int)):
+
+        for index in range(self.CLUSTERS):
             for i in range(size):
                 for j in range(size):
-                    img.putpixel((i+size*index, j), tuple(color))
-        img.show()
+                    img.putpixel((i+size*index, j), tuple(colors[index]))
 
+        if save:
+            img.save(save)
+        else:
+            img.show()
 
 
 img = 'img1.jpg'
 clusters = 10
 
-p = Palette(img, clusters) 
-colors = p.dominantColors()
-p.plotClusters()
-#p.drawPalette(100)
-#print(colors)
-
+p = Palette(img, clusters, downsize=0.01) 
+colors = p.dominantColors(neutral=False)
+p.plotClusters(save='img1-clusters.png')
+p.drawPalette(100, save='img1-palette.png')
+print(colors)
 
